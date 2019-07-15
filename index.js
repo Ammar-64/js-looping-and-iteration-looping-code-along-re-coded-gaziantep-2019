8575dfd48330702a395b3a1224168caa642f76b2
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-15 00:45:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function writeCards (names, event){
   }
   return messages;
 }
+
+function countdown(number){
+  number < 0 ? number *= -1 : number;
+  while (number >= 0 ){
+    console.log(number);
+    number--;
+  }
+}
