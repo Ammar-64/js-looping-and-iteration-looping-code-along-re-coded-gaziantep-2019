a36fa57fbc44bb91768ac752bac581c01ed7f1e0
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-15 00:41:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+function writeCards (names, event){
+  const message = [];
+  for (let i = 0; i < message.length; i++){
+    console.log(`Thank you, ${names[i]}, for the wonderful ${event[i]} gift.``);
+  }
+}
