55f07879c761cf072f13634ef0d2ba6e1c354c64
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-15 00:46:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards (names, event){
+  const messages = [];
+  for (let i = 0; i < message.length; i++){
+    messages[i] = (`Thank you, ${names[i]}, for the wonderful ${event[i]} gift.`);
+  }
+  return messages;
+}
+
+function countdown(number){
+  number < 0 ? number *= -1 : number;
+  while (number >= 0 ){
+    console.log(number);
+    number--;
+  }
+}
